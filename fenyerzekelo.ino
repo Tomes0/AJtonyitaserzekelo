56cbb92a7c2ffb398f0920ca897003737792cbab
tomes <32511604+Tomes0@users.noreply.github.com>
2017-10-04 20:21:06 +0300

    Add files via upload

---
+++ fenyerzekelo.ino
@@ -0,0 +1,18 @@
+int led=6;
+int val;
+void setup() {
+  pinMode(led,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  val=analogRead(4);
+  Serial.println(val,DEC);
+  delay(200);
+  if(val<200){
+    digitalWrite(led,HIGH);
+  }else{
+    digitalWrite(led,LOW);
+  }
+}
+
